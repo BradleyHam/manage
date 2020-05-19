3f1317cad45649e0220a63a08ac9dd56d16c93ed
bradleyham <bradleyjamesham@gmail.com>
2020-05-19 20:41:43 +1200

    add whats different section -mobile

--- src/components/WhatsDifferent/WhatsDifferent.js
+++ src/components/WhatsDifferent/WhatsDifferent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './WhatsDifferent.scss'
+import Button from '../../components/Button/Button'
 
 const WhatsDifferent = () => {
     return (
@@ -7,6 +8,26 @@ const WhatsDifferent = () => {
             <h1>Whats Different about Manage</h1>
             <p>  Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made
   for modern digital product teams.</p>
+
+            <div className="differentOne different">
+                <div className="reasonOne reason"><strong>  Track company-wide progress </strong> <div className="reasonBackground"></div></div>
+                <p> See how your day-to-day tasks fit into the wider vision.
+                Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger
+                picture again.</p>
+            </div>
+
+            <div className="differentTwo different">
+                <div className="reasonTwo reason"><strong>  Advanced built-in reports </strong> <div className="reasonBackground"></div></div>
+                <p>  Set internal delivery estimates and track progress toward company goals. Our
+  customisable dashboard helps you build out the reports you need to keep key stakeholders informed.</p>
+            </div>
+
+            <div className="differentThree different">
+                <div className="reasonThree reason"><strong> Everything you need
+  in one place </strong> <div className="reasonBackground"></div></div>
+                <p>   Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage
+  offers an all-in-one team productivity solution</p>
+            </div>
 
         </div>
     );
