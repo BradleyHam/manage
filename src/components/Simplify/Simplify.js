02e73bd9420125b79cacde8e5429e2894386bfdd
bradleyham <bradleyjamesham@gmail.com>
2020-05-22 11:29:01 +1200

    add Simplify section desktop

--- src/components/Simplify/Simplify.js
+++ src/components/Simplify/Simplify.js
@@ -6,7 +6,8 @@ const Simplify = () => {
         <div className="Simplify">
             <h1>Simplify how your team works today</h1>
             <button className="secondaryButton">Get Started</button>
-            <img src={require('../../images/bg-simplify-section-mobile.svg')} alt="" />
+            <img className='Simplify__mobileImage' src={require('../../images/bg-simplify-section-mobile.svg')} alt="" />
+            <img className='Simplify__desktopImage' src={require('../../images/bg-simplify-section-desktop.svg')} alt="" alt="" />
         </div>
     );
 }
