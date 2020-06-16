796a2daf55e0ac6b748c9bad0298aa528cda0e33
bradleyham <bradleyjamesham@gmail.com>
2020-06-16 21:00:11 +1200

    deploy ready

---
+++ build/precache-manifest.3f711c5cb4719620ecdc95293aff1db4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7769f1ad4af5c8f3ac871cb46d57cba",
+    "url": "/manage/index.html"
+  },
+  {
+    "revision": "eca76f61bce4257e5994",
+    "url": "/manage/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "7583656648f527ddcffa",
+    "url": "/manage/static/css/main.c07743bf.chunk.css"
+  },
+  {
+    "revision": "eca76f61bce4257e5994",
+    "url": "/manage/static/js/2.8798d5ea.chunk.js"
+  },
+  {
+    "revision": "090bf278bc60835529ff324f671c5e50",
+    "url": "/manage/static/js/2.8798d5ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7583656648f527ddcffa",
+    "url": "/manage/static/js/main.f56e9c57.chunk.js"
+  },
+  {
+    "revision": "f8d6587ae118a9576863",
+    "url": "/manage/static/js/runtime-main.5e75f522.js"
+  },
+  {
+    "revision": "d022b688c087eb4bc54fa88f43ccb1be",
+    "url": "/manage/static/media/avatar-ali.d022b688.png"
+  },
+  {
+    "revision": "d591dc600c0ee916d50094bdc81d8902",
+    "url": "/manage/static/media/avatar-anisha.d591dc60.png"
+  },
+  {
+    "revision": "a2c4b4309f9936736240c6d7805c7ccf",
+    "url": "/manage/static/media/avatar-richard.a2c4b430.png"
+  },
+  {
+    "revision": "b2507cb2cb85266d8f2051f637b744aa",
+    "url": "/manage/static/media/avatar-shanai.b2507cb2.png"
+  },
+  {
+    "revision": "d6d0cb363a1a3801a691e7562e2911d2",
+    "url": "/manage/static/media/bg-simplify-section-desktop.d6d0cb36.svg"
+  },
+  {
+    "revision": "37dc03724048fd8cfc2681353ff9c84a",
+    "url": "/manage/static/media/bg-simplify-section-mobile.37dc0372.svg"
+  },
+  {
+    "revision": "010617910cf89bbbf2df1f8bf2340912",
+    "url": "/manage/static/media/bg-tablet-pattern.01061791.svg"
+  },
+  {
+    "revision": "57a86b4fa04731f645568675c17138bd",
+    "url": "/manage/static/media/icon-close.57a86b4f.svg"
+  },
+  {
+    "revision": "bdd40cf9386a83e11660fab1e8fe9965",
+    "url": "/manage/static/media/icon-hamburger.bdd40cf9.svg"
+  },
+  {
+    "revision": "6a53bba8095e0ddefa1791bbd03cf51f",
+    "url": "/manage/static/media/illustration-intro.6a53bba8.svg"
+  },
+  {
+    "revision": "f2cec4b29d7e7f388db937e3f5d9d873",
+    "url": "/manage/static/media/logo.f2cec4b2.svg"
+  }
+]);
